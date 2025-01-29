c014d104fe37aaf9fbb75d20cac16c76ff3a8e5b
Reza <rezasedighi61@gmail.com>
2025-01-29 13:07:38 +0330

    fix build error

--- packages/shared_modules/index.ts
+++ packages/shared_modules/index.ts
@@ -3,8 +3,6 @@ export { default as SidePanelHeader } from "./userSidePanel/header";
 
 export { ModalsList } from "./common/modal/modalsList";
 
-export { Register } from "./userSidePanel/modal_components/register";
 
-export { default as CheckoutPage } from "./checkout/components/page";
 
 export { default as CartItem } from "./checkout/components/cart/item/index";
