616c06ecf0f8b96331fbd16867baf684c2314d02
Mohamad Ganji <mohamad8824@gmail.com>
2025-03-06 16:13:25 +0330

    Update next.config.js

--- apps/learn/next.config.js
+++ apps/learn/next.config.js
@@ -45,6 +45,10 @@ export default withPWA({
         source: "/mag/:path*",
         destination: `https://mag.doctorabad.com/:path*`,
       },
+      {
+        source: "/learn/:path*",
+        destination: `http://localhost:3007/learn/:path*`,
+      },
     ];
   },
 });
