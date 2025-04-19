fd94ceb54b7786e5784c1f41b507f0cc591db142
Erfan Sedighi <erfansedighi84@gmail.com>
2025-04-19 16:07:12 +0330

    fixes some minor DurationFormat bug

--- apps/learn/utils/formatDuration.ts
+++ apps/learn/utils/formatDuration.ts
@@ -6,7 +6,7 @@ const formatDuration = (totalSeconds: number): string => {
   const seconds = totalSeconds % 60;
 
   // return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 };
 
 export default formatDuration;
