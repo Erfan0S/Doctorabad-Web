c36ef16bd8ac3124356a60227557b2c63b6eee91
Mohamad Ganji <mohamad8824@gmail.com>
2025-05-27 12:59:09 +0330

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,23 @@
 module.exports = {
     apps: [
         {
+            name: "mono-base",
+            script: "npm",
+            args: "start",
+            cwd: "/var/www/develop/doctorabad/apps/base",
+            env: {
+                NODE_ENV: "production",
+                PORT: 3001 // Choose different port for each app
+            }
+        },
+        {
             name: "mono-market",
             script: "npm",
             args: "start",
             cwd: "/var/www/develop/doctorabad/apps/market",
             env: {
                 NODE_ENV: "production",
-                PORT: 3005 // Choose different port for each app
+                PORT: 3002 // Choose different port for each app
             }
         },
         {
@@ -17,7 +27,7 @@ module.exports = {
             cwd: "/var/www/develop/doctorabad/apps/learn",
             env: {
                 NODE_ENV: "production",
-                PORT: 3007
+                PORT: 3003
             }
         }
     ]
