e920292d1ffe4bc5f62fb026e39730cca9f3b04e
Mohamad Ganji <mohamad8824@gmail.com>
2025-07-17 11:28:12 +0330

    Update constants.ts

--- packages/core/src/constants/constants.ts
+++ packages/core/src/constants/constants.ts
@@ -1,10 +1,10 @@
 export const defaultBaseUrl =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api"
+    ? "http://localhost:3001/api"
     : "https://develop.doctorabad.com/api";
 // process.env.NODE_ENV === "development"
-//   ? "http://localhost:3000/api"
-//   : "http://localhost:3000/api";
+//   ? "http://localhost:3001/api"
+//   : "http://localhost:3001/api";
 
 export const isServerSide = typeof window === "undefined";
 
