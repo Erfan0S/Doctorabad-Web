f8be43e9cd6a86abc9a632a59fd45c9a2c03e2a4
Mohamad Ganji <mohamad8824@gmail.com>
2025-11-16 14:15:55 +0330

    Update routePath.ts

--- packages/core/src/constants/routePath.ts
+++ packages/core/src/constants/routePath.ts
@@ -33,28 +33,28 @@ export const examPaths = {
 
 export const productionBaseUrl = "https://develop.doctorabad.com";
 
-export const baseUrls = {
-  [Apps.BASE]:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3001"
-      : productionBaseUrl,
-  [Apps.LEARN]:
-    (process.env.NODE_ENV === "development"
-      ? "http://localhost:3003"
-      : productionBaseUrl) + routePath.learnBasePath,
-  [Apps.MARKET]:
-    (process.env.NODE_ENV === "development"
-      ? "http://localhost:3002"
-      : productionBaseUrl) + routePath.marketBasePath,
-  [Apps.EXAM]:
-    (process.env.NODE_ENV === "development"
-      ? "http://localhost:3004"
-      : productionBaseUrl) + routePath.examBasePath,
-};
-
 // export const baseUrls = {
-//   [Apps.BASE]: "https://develop.doctorabad.com",
-//   [Apps.LEARN]: "https://develop.doctorabad.com" + routePath.learnBasePath,
-//   [Apps.MARKET]: "https://develop.doctorabad.com" + routePath.marketBasePath,
-//   [Apps.EXAM]: "https://develop.doctorabad.com" + routePath.examBasePath,
+//   [Apps.BASE]:
+//     process.env.NODE_ENV === "development"
+//       ? "http://localhost:3001"
+//       : productionBaseUrl,
+//   [Apps.LEARN]:
+//     (process.env.NODE_ENV === "development"
+//       ? "http://localhost:3003"
+//       : productionBaseUrl) + routePath.learnBasePath,
+//   [Apps.MARKET]:
+//     (process.env.NODE_ENV === "development"
+//       ? "http://localhost:3002"
+//       : productionBaseUrl) + routePath.marketBasePath,
+//   [Apps.EXAM]:
+//     (process.env.NODE_ENV === "development"
+//       ? "http://localhost:3004"
+//       : productionBaseUrl) + routePath.examBasePath,
 // };
+
+export const baseUrls = {
+  [Apps.BASE]: "https://develop.doctorabad.com",
+  [Apps.LEARN]: "https://develop.doctorabad.com" + routePath.learnBasePath,
+  [Apps.MARKET]: "https://develop.doctorabad.com" + routePath.marketBasePath,
+  [Apps.EXAM]: "https://develop.doctorabad.com" + routePath.examBasePath,
+};
