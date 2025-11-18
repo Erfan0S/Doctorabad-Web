530f344c95def95fbd57a1c789d61f803d84d957
Mohamad Ganji <mohamad8824@gmail.com>
2025-11-18 18:18:48 +0330

    Update constants.ts

--- packages/core/src/constants/constants.ts
+++ packages/core/src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const defaultBaseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3004/api"
-    : "https://develop.doctorabad.com/api";
+    : "https://doctorabad.com/api";
 
 export const isServerSide = typeof window === "undefined";
 
@@ -9,5 +9,5 @@ export const AUTH_COOKIE_KEY = "DALoginStatus";
 
 export const IS_INSTALL_BANNER_SHOW_LOCAL = "WebAppGuideShowed";
 
-// export const API_DESTINATION = "https://drabadapp.ir/:path*";
-export const API_DESTINATION = "http://185.231.180.170/:path*";
+export const API_DESTINATION = "https://drabadapp.ir/:path*";
+// export const API_DESTINATION = "http://185.231.180.170/:path*";
