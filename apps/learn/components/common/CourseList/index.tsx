f36330573f64fc7bb4dbe6f4e54c76d8aaef4e03
Erfan <erfansedighi84@gmail.com>
2026-02-16 17:19:07 +0330

    feat: implement mobile product list components and integrate with course listing

--- apps/learn/components/common/CourseList/index.tsx
+++ apps/learn/components/common/CourseList/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./CourseList.module.scss";
 import { CourseListItemType } from "@/types/courses";
-import CourseListItem from "@/components/common/CourseList/CourseListItem";
-import Link from "next/link";
 import { InfiniteData } from "@tanstack/react-query";
-import InfiniteScroll from "react-infinite-scroller";
-import { PaginatedResponse } from "@repo/core/types/general";
-import Loading from "../Loading";
+import { Apps, PaginatedResponse } from "@repo/core/types/general";
+import { ProductList } from "@repo/shared_modules/components";
+import Clock from "@/assets/svg/clock";
+import formatDuration from "@/utils/formatDuration";
+import { CoinIcon, HomeIcon } from "@repo/shared_modules/icons";
+import { priceFormatter } from "@repo/core/utils/priceFormatter";
+import Hat from "@repo/shared_modules/icons/hat";
+import { ProductListItemProps } from "@repo/core/types/props";
 
 interface Props {
   courses:
@@ -16,24 +19,68 @@ interface Props {
   hasNextPage: boolean;
 }
 
+export const productData = (
+  course: CourseListItemType,
+): ProductListItemProps => {
+  return {
+    id: course.id.toString(),
+    title: course.title,
+    pic_url: course.pic_url,
+    baseUrl: "course",
+    attributes: [
+      {
+        icon: <Clock fontSize={16} />,
+        value: `${formatDuration(course.duration)} ساعت`,
+      },
+      {
+        icon: <CoinIcon fontSize={16} />,
+        value: course.price_off ? priceFormatter(course.price_off) : null,
+      },
+      {
+        icon: <Hat fontSize={16} />,
+        value: course.provider.name,
+      },
+      {
+        icon: <CoinIcon fontSize={16} />,
+        value: (
+          <>
+            <span
+              style={{
+                textDecoration: course.price_off ? "line-through" : "",
+              }}
+            >
+              {!(course.price_main <= 0)
+                ? priceFormatter(course.price_main) + " تومن"
+                : "رایگان"}
+            </span>
+            {course.price_off ? (
+              <span>{priceFormatter(course.price_off)} تومن</span>
+            ) : null}
+          </>
+        ),
+      },
+      {
+        icon: <HomeIcon fontSize={16} />,
+        value: `${course.student_count} دانشجو`,
+      },
+    ],
+    installmentPayment: course.installment_payment,
+    lang: course.language == 1 ? "Fa" : "En",
+  };
+};
+
 const CourseList = ({ courses, fetchNextPage, hasNextPage }: Props) => {
+  const courseData = courses?.pages.flatMap((page) => page.data);
+
   return (
     <div className={styles.relatedCoursesWrapper}>
-      <InfiniteScroll
-        loadMore={() => fetchNextPage()}
-        hasMore={hasNextPage}
-        loader={<Loading />}
-      >
-        {courses?.pages.map((page, i) => (
-          <React.Fragment key={i}>
-            {page.data.map((course) => (
-              <Link href={`/course/${course.id}`} key={course.id}>
-                <CourseListItem course={course} />
-              </Link>
-            ))}
-          </React.Fragment>
-        ))}
-      </InfiniteScroll>
+      <ProductList
+        products={courseData?.map((course) => productData(course))}
+        app={Apps.LEARN}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        emptyErrorMassage="هیچ دوره‌ای یافت نشد"
+      />
     </div>
   );
 };
--- packages/core/src/types/props.ts
+++ packages/core/src/types/props.ts
@@ -3,3 +3,17 @@ import { Apps } from "./general";
 export type BaseUiProps = {
   app?: Apps;
 };
+
+export type ProductListItemProps = {
+  lang?: "Fa" | "En" | null;
+  title: string;
+  id: string;
+  pic_url?: string;
+  baseUrl: string;
+  attributes?: {
+    value?: string | number | React.ReactNode | null;
+    icon?: React.ReactNode;
+  }[];
+  installmentPayment?: boolean;
+  providerTitle?: string;
+};
