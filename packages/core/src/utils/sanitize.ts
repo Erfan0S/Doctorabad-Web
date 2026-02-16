12e0bacf6264fb81df53be0c7830556cbdeed146
Erfan <erfansedighi84@gmail.com>
2026-02-16 15:44:10 +0330

    feat: replace DOMPurify with isomorphic-dompurify and update related components

--- packages/core/src/utils/sanitize.ts
+++ packages/core/src/utils/sanitize.ts
@@ -1,14 +1,5 @@
-import { JSDOM } from "jsdom";
-import DOMPurify from "dompurify";
-import { isServerSide } from "../constants/constants";
-
-const window = new JSDOM("").window;
-const purify = DOMPurify(window);
+import DOMPurify, { clearWindow } from "isomorphic-dompurify";
 
 export default function sanitize(html: string) {
-  if (isServerSide) {
-    return purify.sanitize(html);
-  }
-
   return DOMPurify.sanitize(html);
 }
