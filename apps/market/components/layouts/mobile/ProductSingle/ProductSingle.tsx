e01d5969944f9af0a9eecd1ae54fc291c13839eb
Erfan <erfansedighi84@gmail.com>
2026-05-31 13:40:32 +0330

    feat: add isBundledWithNonProducts utility and update product components to handle bundled products

--- apps/market/components/layouts/mobile/ProductSingle/ProductSingle.tsx
+++ apps/market/components/layouts/mobile/ProductSingle/ProductSingle.tsx
@@ -17,6 +17,7 @@ import MobileProductVariantButton from "@/components/product/mobileLayout/Mobile
 import ProductSlider from "@/components/product/intro/slider";
 import styles from "./ProductSingle.module.scss";
 import bundleProviderImagefrom from "@repo/shared_modules/images/bundel_provider.jpg";
+import { isBundledWithNonProducts } from "@/utils/isBundledWithNonProducts";
 
 const TabsConfig = (
   data: SingleProduct,
@@ -106,7 +107,7 @@ function MobileProductSingle({
           amazingPrice: data.price_amazing,
           productId: data.id,
           orderType: OrderType.ShopProduct,
-          canIncrease: true,
+          canIncrease: !isBundledWithNonProducts(data),
           replaceButton: haveVariant ? (
             <MobileProductVariantButton product={data} />
           ) : undefined,
