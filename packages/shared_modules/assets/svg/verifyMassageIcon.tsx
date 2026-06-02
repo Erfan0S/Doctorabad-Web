904833c6cf924bc940c660df34eee9fbb0d2b1d6
Erfan <erfansedighi84@gmail.com>
2026-06-02 15:51:02 +0330

    feat: enhance VerifyPhone component style with resend functionality and update VerifyMassageIcon

--- packages/shared_modules/assets/svg/verifyMassageIcon.tsx
+++ packages/shared_modules/assets/svg/verifyMassageIcon.tsx
@@ -3,17 +3,33 @@ import { SVGProps } from "react";
 const VerifyMassageIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="currentColor"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
-        d="M14.75 0.75H6.75C2.75 0.75 0.75 2.75 0.75 6.75V19.75C0.75 20.3 1.2 20.75 1.75 20.75H14.75C18.75 20.75 20.75 18.75 20.75 14.75V6.75C20.75 2.75 18.75 0.75 14.75 0.75Z"
-        stroke="#292D32"
+        d="M16 2H8C4 2 2 4 2 8V21C2 21.55 2.45 22 3 22H16C20 22 22 20 22 16V8C22 4 20 2 16 2Z"
+        stroke="currentColor"
         stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7 9.5H17"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7 14.5H14"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
