7e49b9babcaee0ba34664ccdd066c524b7e914c5
Erfan <erfansedighi84@gmail.com>
2026-06-23 17:33:36 +0330

    feat: refactor Providers component and add RuntimeErrorWatcher for improved error handling fix improve page style

--- apps/base/providers/providers.tsx
+++ apps/base/providers/providers.tsx
@@ -1,24 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
-import { SidebarProvider } from "@repo/shared_modules";
-import { QueryClient } from "@tanstack/react-query";
+import React from "react";
 import { ModalsList } from "@repo/shared_modules/modalsList";
 import { Providers as SharedProviders } from "@repo/shared_modules";
-import { isUserLoggedIn } from "@repo/core/utils/authUtils";
-import DiviceSwitchShell from "@repo/shared_modules/components/DiviceSwitchShell";
-import { usePathname } from "next/navigation";
 
 const Providers = ({ children }: React.PropsWithChildren) => {
-  const [client] = useState(
-    new QueryClient({
-      defaultOptions: { queries: { retry: 3, refetchOnWindowFocus: false } },
-    }),
-  );
-  const isLoggedIn = isUserLoggedIn();
-  const pathname = usePathname();
-  const isHome = pathname === "/";
-
   return (
     <SharedProviders haveSideBar modalList={ModalsList}>
       {/* {isHome && isLoggedIn ? null : <SidebarProvider />} */}
