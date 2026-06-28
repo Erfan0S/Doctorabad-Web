184a9cc1421431204aaca283b21f16d59e288021
Reza Charrakh <116076795+RezaCharrakh@users.noreply.github.com>
2026-06-28 11:06:22 +0330

    updating footer address and error handling in market mobile

--- apps/market/components/marketHome/intro/orderInformation/OrderInformationWithFetch.tsx
+++ apps/market/components/marketHome/intro/orderInformation/OrderInformationWithFetch.tsx
@@ -5,18 +5,23 @@ import React from "react";
 import OrderInformation from ".";
 import { isUserLoggedIn } from "@repo/core/utils/authUtils";
 import { useCart } from "@repo/core/states/cart";
+import { OrderType } from "@repo/core/types/cart";
 
 function OrderInformationWithFetch() {
   const cart = useCart();
   const cartIsEmpty = cart.data.length == 0;
+  const isMarketCart = cart.data.every(
+    (item) => item.product_type !== OrderType.ShopProduct,
+  );
+  const shouldFetchOrderInfo = isUserLoggedIn() && !cartIsEmpty && !isMarketCart;
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ["OrderInformation"],
     queryFn: () => api.getLastProcessingOrder().catch(() => null),
-    enabled: isUserLoggedIn() && !cartIsEmpty,
+    enabled: shouldFetchOrderInfo,
   });
 
-  if (isError || isLoading || !data?.data || !isUserLoggedIn() || cartIsEmpty) {
+  if (isError || isLoading || !data?.data || !shouldFetchOrderInfo) {
     return null;
   }
 
