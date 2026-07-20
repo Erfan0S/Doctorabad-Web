1fc5adff8b5b93a8d1aae8c133e0868b89a35ef5
Erfan <erfansedighi84@gmail.com>
2026-07-20 18:04:11 +0330

    refactor: migrate modal styles from SCSS to Tailwind CSS
    Replaced SCSS module imports and class references with Tailwind CSS utility classes in BuyRecommendationModal and ComingSoonModal components. This change standardizes the styling approach across the modal system, moving from scoped SCSS modules to utility-first Tailwind classes for better maintainability and consistency with the project's styling strategy.

--- packages/shared_modules/common/modal/modals/BuyRecommendationModal/index.tsx
+++ packages/shared_modules/common/modal/modals/BuyRecommendationModal/index.tsx
@@ -9,7 +9,6 @@ import {
   ProductSnappayNotif,
 } from "@repo/shared_modules/components";
 import Image from "next/image";
-import styles from "./BuyRecommendationModal.module.scss";
 import { OrderType } from "@repo/core/types/cart";
 import { useCart } from "@repo/core/states/cart";
 import examIcon from "@repo/shared_modules/images/doctor-exam.png";
@@ -29,7 +28,7 @@ const BuyRecommendationModal = ({
       closeModal={closeModal}
       haveAppIcon
       app={Apps.EXAM}
-      className={styles.buyRecommendationModalWrapper}
+      className="px-0 pt-[60px] max-w-[90vw] [&_img]:w-[80px] [&_img]:h-[80px] [&_p]:font-bold [&_p]:text-[14px] [&_h3]:font-bold [&_h3]:text-[14px] [&_p:last-of-type]:text-[13px] [&_p:last-of-type]:font-normal [&_p:last-of-type]:px-[75px] [&_p:last-of-type]:text-center [&_p:last-of-type]:mt-[15px]"
       submitButton={
         <AddToCartButton
           type={OrderType.Exam}
@@ -48,7 +47,7 @@ const BuyRecommendationModal = ({
       }
     >
       <h3>{exam.title}</h3>
-      <p className={styles.examInfo}>
+      <p className="flex flex-col justify-center items-center leading-[18px]">
         <span>{exam.date}</span>
         <span>{exam.place}</span>
       </p>
@@ -58,12 +57,11 @@ const BuyRecommendationModal = ({
         mainPrice={exam.main_price}
         offPrice={exam.off_price}
         app={Apps.EXAM}
-        className={styles.price}
+        className="mb-0 [&>div]:!justify-center"
       />
       {exam.installment_payment && exam.installment_text && (
         <ProductSnappayNotif
           text={exam.installment_text}
-          className={styles.snappayNotif}
         />
       )}
 
